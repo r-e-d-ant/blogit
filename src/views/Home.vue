e4f55d7601d33bd570a5a4d2c96c42da819595ce
r-e-d-ant <mugishathierry65@gmail.com>
2022-02-12 10:08:30 +0200

    dummy data entered

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,28 @@
 
 export default {
   name: 'Home',
-  components: {}
+  components: {},
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome!",
+        blogPost: "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a Filler Title!",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "This is a Filler Title!",
+          blogHTML: "This is a filler blog post title!",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ]
+    }
+  }
 }
 </script>
