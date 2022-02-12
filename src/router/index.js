3a24c56205f607148f5a00fc779c9bab4b82b64b
r-e-d-ant <mugishathierry65@gmail.com>
2022-02-12 12:14:15 +0200

    initialized blog route

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Blogs from "../views/Blogs.vue"
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
-  }
+  },
+  {
+    path: '/blogs',
+    name: 'Blogs',
+    component: Blogs
+  },
 ]
 
 const router = createRouter({
