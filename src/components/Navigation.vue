fb44db4875ec7df419d389b7a1b9605ffd173576
r-e-d-ant <mugishathierry65@gmail.com>
2022-02-17 11:43:26 +0200

    created create blog blog post view and added to routes and navavigations

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -9,7 +9,7 @@
                <ul v-show="!mobile">
                    <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
                    <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
-                   <router-link v-if="admin" class="link" to="#">Create Post</router-link>
+                   <router-link v-if="admin" class="link" :to="{ name: 'CreatePost' }">Create Post</router-link>
                    <router-link v-if="!user" class="link" :to="{ name: 'Login' }">Login/Register</router-link>
                </ul>
                <div v-if="user" @click="toggleProfileMenu" class="profile" ref="profile">
@@ -48,7 +48,7 @@
            <ul v-show="mobileNav">
                <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
                <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
-               <router-link v-if="admin" class="link" to="#">reate Post</router-link>
+               <router-link v-if="admin" class="link" :to="{ name: 'CreatePost' }">reate Post</router-link>
                <router-link v-if="!user" class="link" :to="{ name: 'Login' }">Login/Register</router-link>
             </ul>
        </transition>
---
+++ src/views/CreatePost.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <div>Create a Post</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
